eafebf22e875d269767b78a839934c343654be5e
Krit <yothinpeace@gmail.com>
2024-07-15 17:49:38 +0700

    ENH: Add simple About section

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <section id="about">
+      <h1>About Me</h1>
+      <p>This is the about section.</p>
+    </section>
+  );
+};
+
+export default About;
