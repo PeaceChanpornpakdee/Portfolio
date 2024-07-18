ccf72348df982ae2eaf7c499e50daace2afb1d39
Krit <yothinpeace@gmail.com>
2024-07-18 16:11:48 +0700

    ENH: Setup Tailwind css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,7 @@ const About: React.FC = () => {
   return (
     <section id="about">
       <h1>About Me</h1>
+      <div className="text-blue-300 text-2xl font-bold"> Nice to meet you</div>
       <p>This is the about section.</p>
     </section>
   );
